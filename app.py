57101271f33f24e1e7ae4976a49eddea00c03d59
heyuoo <ybr2431@hanmail.net>
2024-12-17 21:26:56 +0900

    20

--- app.py
+++ app.py
@@ -56,39 +56,33 @@ with st.sidebar:
         type=["pdf", "txt", "docx"],
     )
 
-    api_key = os.getenv("OPENAI_API_KEY")
+    api_key = st.secrets["OPENAI_API_KEY"]
 
     if not api_key:
-        try:
-            api_key = st.secrets["OPENAI_API_KEY"]
+        api_key = st.sidebar.text_input(
+            "Enter OpenAI API Key", type="password"
+        )
+        st.warning("API Key is required to proceed.")
+        st.markdown(
+            "[🚀View on"
+            "Code](https://github.com/heyuoo/FULLSTACK-GPT/blob/streamlit5/app.py)"
+        )
+        st.stop()
 
-        except KeyError:
-            api_key = st.sidebar.text_input(
-                "Enter OpenAI API Key", type="password"
-            )
+    if len(api_key.strip()) <= 150:
+        st.error("Invalid API Key. Please enter a valid OpenAI API Key.")
+        st.markdown(
+            "[🚀View on"
+            "Code](https://github.com/heyuoo/FULLSTACK-GPT/blob/streamlit5/app.py)"
+        )
+        st.stop()
 
-        if not api_key:
-            st.warning("API Key is required to proceed.")
-            st.markdown(
-                "[🚀View on"
-                "Code](https://github.com/heyuoo/FULLSTACK-GPT/blob/streamlit5/app.py)"
-            )
-            st.stop()
-
-        if len(api_key.strip()) <= 150:
-            st.error("Invalid API Key. Please enter a valid OpenAI API Key.")
-            st.markdown(
-                "[🚀View on"
-                "Code](https://github.com/heyuoo/FULLSTACK-GPT/blob/streamlit5/app.py)"
-            )
-            st.stop()
-
-        else:
-            st.sidebar.success("API Key loaded successfully!")
-            st.markdown(
-                "[🚀View on"
-                "Code](https://github.com/heyuoo/FULLSTACK-GPT/blob/streamlit5/app.py)"
-            )
+    else:
+        st.sidebar.success("API Key loaded successfully!")
+        st.markdown(
+            "[🚀View on"
+            "Code](https://github.com/heyuoo/FULLSTACK-GPT/blob/streamlit5/app.py)"
+        )
 
 
 llm = ChatOpenAI(
