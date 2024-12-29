2649f159b0da618e61cfbdb393d5793a9bb57bcc
heyuoo <ybr2431@hanmail.net>
2024-12-29 13:29:58 +0900

    2-2

--- pages/04_AssistantGPT.py
+++ pages/04_AssistantGPT.py
@@ -275,11 +275,8 @@ if query:
     if "run" in st.session_state:
         del st.session_state["run"]
 
-    paint_message(query, "human")
-
-    result = save_to_txt({"text": f"Research about {query}"})
-
     paint_history()
+    paint_message(query, "human")
 
     thread = client.beta.threads.create(
         messages=[
