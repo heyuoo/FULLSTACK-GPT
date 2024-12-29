f2a885ed7194f431a0daf9964e69af7b1ebbbdb6
heyuoo <ybr2431@hanmail.net>
2024-12-29 15:34:52 +0900

    2-8

--- pages/04_AssistantGPT.py
+++ pages/04_AssistantGPT.py
@@ -287,10 +287,7 @@ if query:
     st.session_state["messages"].append({"message": query, "role": "human"})
     paint_history()  # 최신 대화 기록 출력
 
-    # AI 응답 생성
-    response = f"I am researching {query}..."
-    st.session_state["messages"].append({"message": response, "role": "ai"})
-    paint_history()
+    #
 
     thread = client.beta.threads.create(
         messages=[
